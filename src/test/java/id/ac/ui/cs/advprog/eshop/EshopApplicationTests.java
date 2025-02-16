6374c45a342d2a9f4da916da88a58c780acbb03e
secona <77039267+secona@users.noreply.github.com>
2025-02-16 17:02:02 +0700

    tests: add tests for EshopApplication

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -5,9 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EshopApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testMainMethodWorks() {
+		// This is to test whether the Application errors or not.
+		EshopApplication.main(new String[]{});
+	}
 }
