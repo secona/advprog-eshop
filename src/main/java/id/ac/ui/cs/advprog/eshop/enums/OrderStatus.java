2abca2fabf811fc04338132058a792be22294b34
secona <77039267+secona@users.noreply.github.com>
2025-03-03 16:13:54 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/OrderStatus.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELLED("CANCELLED");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String value) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.name().equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
