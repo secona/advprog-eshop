d4854c502040605a36e674801737905be32cebee
secona <77039267+secona@users.noreply.github.com>
2025-03-06 16:24:12 +0700

    [RED] Add tests for PaymentRepository

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PaymentRepositoryTest {
     PaymentRepository paymentRepository;
     List<Payment> payments;
+    List<Order> orders;
 
     @BeforeEach
     void setUp() {
@@ -30,26 +33,52 @@ public class PaymentRepositoryTest {
         paymentData.put("address", "Jakarta");
         paymentData.put("deliveryFee", "100000");
         payments.add(new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "CASH_ON_DELIVERY", paymentData));
+
+        List<Product> products = new ArrayList<>();
+
+        Product product1 = new Product();
+        product1.setProductId("3e8c353b-ff26-4dae-b925-fb7bd7c807f6");
+        product1.setProductName("Kaoru Hana wa Rin to Saku Vol. 1");
+        product1.setProductQuantity(1);
+
+        products.add(product1);
+
+        orders = new ArrayList<>();
+
+        orders.add(new Order("d4bfe9c1-c0da-4b94-bda4-7921c451ac14", products, 1708560000L, "Guts"));
+        orders.add(new Order("f55e5029-9f30-43e7-87b0-7e979f392c24", products, 1708560000L, "Guts"));
     }
 
     @Test
     void testSaveCreate() {
         Payment payment = payments.getFirst();
-        paymentRepository.save(payment);
+        Order order = orders.getFirst();
+        paymentRepository.save(payment, order);
 
-        Payment findResult = paymentRepository.findById(payment.getId());
-        assertEquals(payment.getId(), findResult.getId());
-        assertEquals(payment.getStatus(), findResult.getStatus());
-        assertEquals(payment.getMethod(), findResult.getMethod());
-        assertSame(payment.getPaymentData(), findResult.getPaymentData());
+        Payment paymentResult = paymentRepository.findById(payment.getId());
+        assertEquals(payment.getId(), paymentResult.getId());
+        assertEquals(payment.getStatus(), paymentResult.getStatus());
+        assertEquals(payment.getMethod(), paymentResult.getMethod());
+        assertSame(payment.getPaymentData(), paymentResult.getPaymentData());
+
+        Order orderResult = paymentRepository.findOrderByPaymentId(payment.getId());
+        assertEquals(order.getId(), orderResult.getId());
 
         assertEquals(1, paymentRepository.findAll().size());
     }
 
     @Test
-    void testSaveUpdate() {
+    void testSaveCreateFailsIfNoOrder() {
         Payment payment = payments.getFirst();
-        paymentRepository.save(payment);
+
+        assertThrows(IllegalArgumentException.class, () -> paymentRepository.save(payment));
+    }
+
+    @Test
+    void testSaveUpdatePayment() {
+        Payment payment = payments.getFirst();
+        Order order = orders.getFirst();
+        paymentRepository.save(payment, order);
 
         Payment newPayment = new Payment(payment.getId(), payments.get(1).getMethod(), payments.get(1).getPaymentData());
         Payment result = paymentRepository.save(newPayment);
@@ -66,8 +95,8 @@ public class PaymentRepositoryTest {
 
     @Test
     void testFindByIdIfIdFound() {
-        for (Payment payment : payments) {
-            paymentRepository.save(payment);
+        for (int i = 0; i < payments.size(); i++) {
+            paymentRepository.save(payments.get(i), orders.get(i));
         }
 
         Payment findResult = paymentRepository.findById(payments.get(1).getId());
@@ -79,8 +108,8 @@ public class PaymentRepositoryTest {
 
     @Test
     void testFindByIdIfIdNotFound() {
-        for (Payment payment : payments) {
-            paymentRepository.save(payment);
+        for (int i = 0; i < payments.size(); i++) {
+            paymentRepository.save(payments.get(i), orders.get(i));
         }
 
         Payment findResult = paymentRepository.findById("asdf");
@@ -89,8 +118,8 @@ public class PaymentRepositoryTest {
 
     @Test
     void testFindAll() {
-        for (Payment payment : payments) {
-            paymentRepository.save(payment);
+        for (int i = 0; i < payments.size(); i++) {
+            paymentRepository.save(payments.get(i), orders.get(i));
         }
 
         List<Payment> findAllResult = paymentRepository.findAll();
