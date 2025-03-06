1af2509d3ccc74c20b7d8060da45e27b80f9bfb5
secona <77039267+secona@users.noreply.github.com>
2025-03-06 16:26:44 +0700

    [GREEN] Implement order and payment in PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,16 +1,29 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Order> orderData = new HashMap<>();
+
+    public Payment save(Payment payment, Order order) {
+        orderData.put(payment.getId(), order);
+        return this.save(payment);
+    }
 
     public Payment save(Payment payment) {
+        if (!orderData.containsKey(payment.getId())) {
+            throw new IllegalArgumentException();
+        }
+
         int i = 0;
 
         for (Payment saved : paymentData) {
@@ -35,6 +48,10 @@ public class PaymentRepository {
         }
 
         return null;
+    }
+
+    public Order findOrderByPaymentId(String paymentId) {
+        return orderData.get(paymentId);
     }
 
     public List<Payment> findAll() {
