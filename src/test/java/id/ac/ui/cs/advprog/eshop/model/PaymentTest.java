bf59e9e4338881a05706892cabe2d93073348263
secona <77039267+secona@users.noreply.github.com>
2025-03-06 09:07:53 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,77 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentTest {
+    @Test
+    void testCreatePaymentByValidVoucherCode() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+
+        Payment payment = new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "VOUCHER_CODE", paymentData);
+
+        assertEquals("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", payment.getId());
+        assertEquals("VOUCHER_CODE", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+        assertSame(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentByInvalidVoucherCode() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "asdf");
+
+        Payment payment = new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "VOUCHER_CODE", paymentData);
+
+        assertEquals("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", payment.getId());
+        assertEquals("VOUCHER_CODE", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertSame(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentByValidCashOnDelivery() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("address", "Jakarta");
+        paymentData.put("deliveryFee", "100000");
+
+        Payment payment = new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "CASH_ON_DELIVERY", paymentData);
+
+        assertEquals("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", payment.getId());
+        assertEquals("CASH_ON_DELIVERY", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+        assertSame(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentByInvalidCashOnDelivery() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("deliveryFee", "100000");
+
+        Payment payment = new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "CASH_ON_DELIVERY", paymentData);
+
+        assertEquals("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", payment.getId());
+        assertEquals("CASH_ON_DELIVERY", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertSame(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentByInvalidCashOnDeliveryEmptyString() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("address", "");
+        paymentData.put("deliveryFee", "100000");
+
+        Payment payment = new Payment("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", "CASH_ON_DELIVERY", paymentData);
+
+        assertEquals("d0ef59ff-7e69-44b6-961b-bdb3c016cc3b", payment.getId());
+        assertEquals("CASH_ON_DELIVERY", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertSame(paymentData, payment.getPaymentData());
+    }
+}
