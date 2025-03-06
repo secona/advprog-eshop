ff098ba405bf39d9dc9ce70b1a99509f5ca3e84e
secona <77039267+secona@users.noreply.github.com>
2025-03-06 10:20:08 +0700

    [GREEN] Implement updating payment in PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,6 +11,18 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int i = 0;
+
+        for (Payment saved : paymentData) {
+            if (saved.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+
+            i += 1;
+        }
+
         paymentData.add(payment);
         return payment;
     }
